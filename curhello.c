55e859bb8667654af641c8a209ccac5ecc34c6e7
Patrick Chen <patrick.chen64@gmail.com>
2015-03-04 01:54:09 -0600

    hi

--- curhello.c
+++ curhello.c
@@ -1,15 +1,3 @@
-/*
-
-  CURHELLO.C
-  ==========
-  (c) Copyright Paul Griffiths 1999
-  Email: mail@paulgriffiths.net
-
-  "Hello, world!", ncurses style.
-
-*/
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>                  /*  for sleep()  */
